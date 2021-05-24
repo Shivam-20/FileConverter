9fc88c798fdd3701c308d1ad054bbec2cd45e430
Shivam <46343363+Shivam-20@users.noreply.github.com>
2021-05-24 10:50:25 +0530

    git deploy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import EncodePassword from "./components/EncodePassword";
-// import DecodeFile from "./components/DecodeFile";
+import DecodeFile from "./components/DecodeFile";
 import HomePage from "./components/HomePage";
 // import FileUploaderCard from "./components/UI Components/FileUploaderCard";
 
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      {/* <DecodeFile />
-      <FileUploaderCard/> */}
-      <EncodePassword />
+      <HomePage />
+      <DecodeFile />
+      {/* <FileUploaderCard/> */}
+      {/* <EncodePassword /> */}
     </>
   );
 }
